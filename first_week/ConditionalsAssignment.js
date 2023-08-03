db22931267092050de751f42a8f20341566d6432
Francis Ngo <fngo@LABA.PSWAS.LOCAL>
2023-08-03 15:49:26 -0700

    Second Commit

--- first_week/ConditionalsAssignment.js
+++ first_week/ConditionalsAssignment.js
@@ -1,71 +1,100 @@
-Get Setup
 
-make a .js file in vs code
+let numberOfStudents = 17;
 
-Initialize a git repo ("git init" while inside correct folder)
+//Check if we have more than 20 students, if yes console log "classroom too small"
 
-Make a new Github repo
+//else console log "classroom size acceptable"
 
-connect local repo and Github repo
-
-
-In your primary js file complete the following
-
-
-1. let numberOfStudents = 17;
-
-Check if we have more than 20 students, if yes console log "classroom too small"
-
-else console log "classroom size acceptable"
-
- 
-
-2. 
+if (numberOfStudents >= 20) {
+    numberOfStudents = "classroom too small"
+} else {
+    numberOfStudents ="classroom size acceptable"
+}
+console.log(numberOfStudents)
+  
 
 let numberOfHouses = 5;
 
-Check if this person’s number of houses is greater than 2 - if true console.log(“you need to sell more houses”)
+//Check if this person’s number of houses is greater than 2 - if true console.log(“you need to sell more houses”)
 
-If 2 or less houses - log “seems reasonable”
+//If 2 or less houses - log “seems reasonable”
 
- 
+if (numberOfHouses > 2) {
+    numberOfHouses = "you need to sell more houses"
+ } else {
+    numberOfHouses = "seems reasonable"
+ }
+ console.log(numberOfHouses)
 
-3. 
+
 
 let bill = 20;
 
-Check if the bill is higher than 15 if yes “we should split the bill” 
+//Check if the bill is higher than 15 if yes “we should split the bill” 
 
-If not higher than 15 “I’ll get the bill this time”
+//If not higher than 15 “I’ll get the bill this time”
 
- 
-
-
-4. (OPTIONAL) Write an if/else statement for the following requirements:
-
-    If students get 55 or lower: console log "Not to good"
-    If students get 60 or lower: console log "ehhhh"
-    If students get 70 or lower: console log "Keep trying"
-    If students get 80 or lower: console log "nice!"
-    If students get 90 or lower: console log "Doing good!"
-    else console log "over 90! great!"
-
-5. 
-
-I want you to think of 3 more interesting examples using a simple conditional (checking person’s name, checking person’s age, checking something….)
+if (bill > 15) {
+    bill = "we should split the bill"
+} else {
+    bill = "I'll get the bill this time"
+}
+console.log(bill)
 
  
 
+ //(OPTIONAL) Write an if/else statement for the following requirements:
+
+    //If students get 55 or lower: console log "Not to good"
+    //If students get 60 or lower: console log "ehhhh"
+    //If students get 70 or lower: console log "Keep trying"
+    //If students get 80 or lower: console log "nice!"
+    //If students get 90 or lower: console log "Doing good!"
+    //else console log "over 90! great!"
+
+
+//I want you to think of 3 more interesting examples using a simple conditional (checking person’s name, checking person’s age, checking something….)
+
+ let learnerAge= 18
+
+ if (learnerAge >= 21) {
+    learnerAge = "Entry into General Bar and Night Club"
+ } else if (learnerAge >= 18) {
+    learnerAge = "Entry to 18+ Club Section Granted, must wear neon wristband and black X's on hands."
+ } else {
+    learnerAge = "Entry NOT permitted into venue. learner is a Minor!"
+ }
+console.log(learnerAge)
  
 
-Complete all work in your js file
+let learnerName= "Francis"
 
-git add .
+if (learnerName = "Francis") {
+    learnerName = "You cannot eat a cookie."
+} else if (learnerName = "Chase") {
+    learnerName = "You may two eat cookies!"
+} else {
+    learnerName = "You may eat a cookie"
+}
+console.log(learnerName)
 
-git commit -m 'finish lab'
+let checkingAccountNumber = 9876543211
 
-git push origin master (or git push origin main)
+if (checkingAccountNumber >= 9000000000 ) {
+    checkingAccountNumber = "Valid"
+} else { 
+    checkingAccountNumber = "Invalid"
+}
+console.log(checkingAccountNumber)
+
+//Complete all work in your js file
+
+//git add .
+
+//git commit -m 'finish lab'
+
+//git push origin master (or git push origin main)
 
  
 
-submit the link to your Github here. //
+//submit the link to your Github here. //
