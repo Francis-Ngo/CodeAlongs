107bb51543a0d95edfa8325f112c55f28c9086e1
Francis Ngo <fngo@LABA.PSWAS.LOCAL>
2023-08-03 14:42:34 -0700

    first commit

---
+++ first_week/ConditionalsAssignment.js
@@ -0,0 +1,71 @@
+Get Setup
+
+make a .js file in vs code
+
+Initialize a git repo ("git init" while inside correct folder)
+
+Make a new Github repo
+
+connect local repo and Github repo
+
+
+In your primary js file complete the following
+
+
+1. let numberOfStudents = 17;
+
+Check if we have more than 20 students, if yes console log "classroom too small"
+
+else console log "classroom size acceptable"
+
+ 
+
+2. 
+
+let numberOfHouses = 5;
+
+Check if this person’s number of houses is greater than 2 - if true console.log(“you need to sell more houses”)
+
+If 2 or less houses - log “seems reasonable”
+
+ 
+
+3. 
+
+let bill = 20;
+
+Check if the bill is higher than 15 if yes “we should split the bill” 
+
+If not higher than 15 “I’ll get the bill this time”
+
+ 
+
+
+4. (OPTIONAL) Write an if/else statement for the following requirements:
+
+    If students get 55 or lower: console log "Not to good"
+    If students get 60 or lower: console log "ehhhh"
+    If students get 70 or lower: console log "Keep trying"
+    If students get 80 or lower: console log "nice!"
+    If students get 90 or lower: console log "Doing good!"
+    else console log "over 90! great!"
+
+5. 
+
+I want you to think of 3 more interesting examples using a simple conditional (checking person’s name, checking person’s age, checking something….)
+
+ 
+
+ 
+
+Complete all work in your js file
+
+git add .
+
+git commit -m 'finish lab'
+
+git push origin master (or git push origin main)
+
+ 
+
+submit the link to your Github here. //
